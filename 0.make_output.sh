d2dd2d8a09b1688fa5f8c9311ef3056790996fde
Hyun-sa <chminoo@g.cbnu.ac.kr>
2023-08-09 15:59:25 +0900

    0.1.1. MakeOutput Update

--- 0.make_output.sh
+++ 0.make_output.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
+
+if [[ $1 == "C" ]]; then
+  compile_extension=".c"
+  compile_command="gcc -o"
+elif [[ $1 == "Python" ]]; then
+  compile_extension=".py"
+  compile_command="python3 -m py_compile"
+else
+  echo "Please Type "C" or "Python"" >&2
+  exit 1
+fi
+
+
 for file_path in ./input/*
 do
+  if [[ "${file_path}" != *"${compile_extension}" ]]; then
+    continue
+  fi
   if [ -f "$file_path" ]
   then
     file_name=$(basename "$file_path")
     mkdir -p "./output/${file_name%.*}"
-    gcc -o "./output/${file_name%.*}/${file_name%.*}" "$file_path"
+    
+    if [[ $1 == "C" ]]; then
+      $compile_command "./output/${file_name%.*}/${file_name%.*}" "$file_path"
+    fi
+    
     for i in {1..50}
     do
-      echo $i | "./output/${file_name%.*}/${file_name%.*}" > "./output/${file_name%.*}/$i.txt"
+      if [[ $1 == "C" ]]; then
+        echo "$i" | "./output/${file_name%.*}/${file_name%.*}" > "./output/${file_name%.*}/result_$i.txt"
+      elif [[ $1 == "Python" ]]; then
+        echo "$i" | python3 "$file_path" > "./output/${file_name%.*}/result_$i.txt"
+      fi
     done
   fi
 done
