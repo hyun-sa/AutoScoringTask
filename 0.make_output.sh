b02df83f38ccaef21aed96b363bd9e1e569d03c5
Hyun-sa <chminoo@g.cbnu.ac.kr>
2023-08-13 14:14:36 +0900

    changes style

--- 0.make_output.sh
+++ 0.make_output.sh
@@ -11,7 +11,6 @@ else
   exit 1
 fi
 
-
 for file_path in ./input/*
 do
   if [[ "${file_path}" != *"${compile_extension}" ]]; then
@@ -20,20 +19,23 @@ do
   if [ -f "$file_path" ]
   then
     file_name=$(basename "$file_path")
-    rm -rf "./output/${file_name%.*}"
-    mkdir -p "./output/${file_name%.*}"
+    rm -rf "./output/${file_name%.*}.txt"
+    mkdir -p "./output"
     
     if [[ $1 == "C" ]]; then
-      $compile_command "./output/${file_name%.*}/${file_name%.*}" "$file_path"
+      $compile_command "./output/${file_name%.*}" "$file_path"
     fi
     
     for i in {1..2000}
     do
       if [[ $1 == "C" ]]; then
-        echo "$i" | "./output/${file_name%.*}/${file_name%.*}" > "./output/${file_name%.*}/result_$i.txt"
+        echo "<<< $i case >>>" >> "./output/${file_name%.*}.txt"
+        echo "$i" | "./output/${file_name%.*}" >> "./output/${file_name%.*}.txt"        
       elif [[ $1 == "Python" ]]; then
-        echo "$i" | python3 "$file_path" > "./output/${file_name%.*}/result_$i.txt"
+        echo "<<< $i case >>>" >> "./output/${file_name%.*}.txt"
+        echo "$i" | python3 "$file_path" >> "./output/${file_name%.*}.txt"
       fi
     done
   fi
 done
+
--- 1.check_cases.py
+++ 1.check_cases.py
@@ -1,17 +1,19 @@
 import os
 
-
 def compare_results(case_name, total_cases):
     answer = 0
     error_cases = []
 
-    for i in range(1, total_cases + 1):
-        result_file = f'./output/{case_name}/result_{i}.txt'
-        answer_file = f'./output/answer/result_{i}.txt'
+    result_file = f'./output/{case_name}.txt'
+    answer_file = f'./output/answer.txt'
 
-        with open(result_file, 'r') as r_file, open(answer_file, 'r') as a_file:
-            result_content = r_file.read().strip()
-            answer_content = a_file.read().strip()
+    with open(result_file, 'r') as r_file, open(answer_file, 'r') as a_file:
+        lines = r_file.readlines()
+        answer_lines = a_file.readlines()
+
+        for i in range(1, total_cases + 1):
+            result_content = lines[(i - 1) * 2 + 1].strip()
+            answer_content = answer_lines[(i - 1) * 2 + 1].strip()
 
             if result_content == answer_content:
                 answer += 1
@@ -19,7 +21,7 @@ def compare_results(case_name, total_cases):
                 error_cases.append(i)
 
     percentage = (answer / total_cases) * 100
-    summary = f"{answer} / {total_cases}\n{percentage:.2f}%\nerror cases : {*error_cases}\n"
+    summary = f"{answer}/{total_cases}\n{percentage:.2f}%\n\/ error cases : {error_cases}\n"
 
     with open(f'./result/{case_name}.txt', 'w') as result_file:
         result_file.write(summary)
@@ -30,25 +32,26 @@ def get_case_names():
     case_names = []
 
     for entry in os.scandir(output_path):
-        if entry.is_dir() and entry.name != 'answer':
-            case_names.append(entry.name)
+        if entry.is_file() and entry.name.endswith('.txt'):
+            case_names.append(entry.name[:-4])
 
     return case_names
 
 
-def count_txt_files(path):
-    txt_count = 0
+def count_cases_in_txt(path):
+    case_count = 0
 
-    for entry in os.scandir(path):
-        if entry.is_file() and entry.name.endswith('.txt'):
-            txt_count += 1
+    with open(path, 'r') as f:
+        for line in f:
+            if '<<<' in line and 'case >>>' in line:
+                case_count += 1
 
-    return txt_count
+    return case_count
 
 
 case_names = get_case_names()
-    
+
 for case_name in case_names:
-    total_cases = count_txt_files('./output/answer')
+    total_cases = count_cases_in_txt('./output/answer.txt')
     compare_results(case_name, total_cases)
 
